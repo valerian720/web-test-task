90ac9095f36a5e0f66ed47521c40956822592937
Валериан Воротников <valerian7202@gmail.com>
2023-03-24 15:26:30 +0300

    1 task

--- 1 task/index.js
+++ 1 task/index.js
@@ -2,17 +2,32 @@ const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
+let trackedDatetime = new Date();
+
+const msInSecond = 1000;
+
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  UpdateTimer();
+  return (seconds) => {
+    var timerId = setInterval(UpdateTimer, msInSecond)
+    setTimeout(() => clearTimeout(timerId), seconds * msInSecond);
+  };
 };
+
+function UpdateTimer() {
+  trackedDatetime = new Date(trackedDatetime.getTime() + msInSecond);
+  timerEl.textContent = `${trackedDatetime.toLocaleTimeString("ru-ru")}`; // display tracked time
+  // timerEl.textContent = `${new Date().toLocaleTimeString("ru-ru")}`; // always current time
+}
 
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener('input', () => {
+inputEl.addEventListener('input', (e) => {
   // Очистите input так, чтобы в значении
   // оставались только числа
+  inputEl.value = inputEl.value.replace(/[^0-9]/g, '');;
 });
 
 buttonEl.addEventListener('click', () => {
